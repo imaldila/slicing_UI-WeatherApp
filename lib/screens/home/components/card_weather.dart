006339f66e640ac326067e670eef1cf55d688584
Apit <apit@thezatapp.com>
2022-04-15 21:33:35 +0700

    refractor card_weather

--- lib/screens/home/components/card_weather.dart
+++ lib/screens/home/components/card_weather.dart
@@ -13,68 +13,94 @@ class CardWeather extends StatelessWidget {
       height: 335,
       width: double.infinity,
       decoration: kBoxDecoration,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            'Today, 14 April 2022',
-            style: kTextStyleMap18,
-          ),
-          Text(
-            '29°',
-            style: kTextStyleMap100,
-          ),
-          Text(
-            'Cloudy',
-            style: kTextStyleMap18,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset('assets/icons/ic-windy.png'),
-              Text(
-                'Wind',
-                style: kTextStyleMap18,
-              ),
-              Text(
-                '|',
-                style: kTextStyleMap18,
-              ),
-              Text(
-                '10 km/h',
-                style: kTextStyleMap18,
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset('assets/icons/ic-hum.png'),
-              // const SizedBox(
-              //   width: kPadding,
-              // ),
-              Text(
-                'Hum',
-                style: kTextStyleMap18,
-              ),
-              // const SizedBox(
-              //   width: kPadding,
-              // ),
-              Text(
-                '|     ',
-                style: kTextStyleMap18,
-              ),
-              // const SizedBox(
-              //   width: kPadding,
-              // ),
-              Text(
-                '54 %',
-                style: kTextStyleMap18,
-              )
-            ],
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: kPadding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Today, 14 April 2022',
+              style: kTextStyleMap18,
+            ),
+            Text(
+              '29°',
+              style: kTextStyleMap100,
+            ),
+            Text(
+              'Cloudy',
+              style: kTextStyleMap24,
+            ),
+            const SizedBox(
+              height: kPadding,
+            ),
+            const WeatherAttribute(
+              image: 'assets/icons/ic-windy.png',
+              title: 'Wind',
+              value: '10 Km/h',
+            ),
+            const SizedBox(
+              height: kPadding / 2,
+            ),
+            const WeatherAttribute(
+              image: 'assets/icons/ic-hum.png',
+              title: 'Hun',
+              value: '54 %',
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class WeatherAttribute extends StatelessWidget {
+  const WeatherAttribute({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  final String image, title, value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset(image),
+              const SizedBox(
+                width: kPadding,
+              ),
+              Text(
+                title,
+                style: kTextStyleMap18,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: kPadding,
+        ),
+        Text(
+          '|',
+          style: kTextStyleMap18,
+        ),
+        const SizedBox(
+          width: kPadding,
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: kTextStyleMap18,
+          ),
+        ),
+      ],
     );
   }
 }
